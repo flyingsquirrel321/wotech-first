e8ba79c13dd55773b55fad780b9bfb23f1ed5a1d
flyingsquirrel321 <165931847+flyingsquirrel321@users.noreply.github.com>
2024-04-03 19:19:28 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+  public static void main(String[] args) {
+
+    // winter, spring, summer, autumn
+    // warm jacket, T-shirt, simming suit, rain coat
+    String season = "randomword";
+    
+      if (season == "winter") {
+        System.out.println("Wear a warm jacket");
+      }
+      else if (season == "spring") {
+        System.out.println("Wear a T-shirt");
+      }
+      else if (season == "summer") {
+        System.out.println("Wear a simming suit");
+      }
+      else if (season == "autumn") {
+        System.out.println("Wear a rain coat");
+      }
+      else {
+        System.out.println("I do not recognize this season!");
+      }
+    
+
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
